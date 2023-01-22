18d2c19bd2a86f7a70f8d0e1515ae09103e7d1fa
Jordan <jdvandenbruel@gmail.com>
2023-01-22 14:35:10 -0700

    initial draft of project

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
-CMPUT404-assignment-webserver
-=============================
+# CMPUT404-assignment-webserver
 
 CMPUT404-assignment-webserver
 
@@ -7,20 +6,34 @@ See requirements.org (plain-text) for a description of the project.
 
 Make a simple webserver.
 
-Contributors / Licensing
-========================
+# Contributors / Licensing
 
 Generally everything is LICENSE'D under the Apache 2 license by Abram Hindle.
 
 server.py contains contributions from:
 
-* Abram Hindle
-* Eddie Antonio Santos
-* Jackson Z Chang
-* Mandy Meindersma 
+- Abram Hindle
+- Eddie Antonio Santos
+- Jackson Z Chang
+- Mandy Meindersma
 
 But the server.py example is derived from the python documentation
 examples thus some of the code is Copyright © 2001-2013 Python
 Software Foundation; All Rights Reserved under the PSF license (GPL
 compatible) http://docs.python.org/2/library/socketserver.html
 
+========================
+
+The website PYnative.com was referenced when searching for a way to
+print the current day of the week as a string. Vishal Hule
+wrote the article.
+
+https://pynative.com/python-get-the-day-of-week/#h-get-the-weekday-name-from-date-using-calendar-module
+
+========================
+
+Miscellaneous Python documentation was referenced when creating this such as the os
+and time librarys.
+
+https://docs.python.org/3/library/os.html
+https://docs.python.org/3/library/time.html
--- server.py
+++ server.py
@@ -1,14 +1,19 @@
-#  coding: utf-8 
+#  coding: utf-8
 import socketserver
+import os
+import time
+import calendar
+
+# http response format: https://developer.mozilla.org/en-US/docs/Web/HTTP/Messages
 
 # Copyright 2013 Abram Hindle, Eddie Antonio Santos
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -28,11 +33,111 @@ import socketserver
 
 
 class MyWebServer(socketserver.BaseRequestHandler):
-    
+
+    # handle an HTTP request
     def handle(self):
         self.data = self.request.recv(1024).strip()
-        print ("Got a request of: %s\n" % self.data)
-        self.request.sendall(bytearray("OK",'utf-8'))
+        print("Got a request of: %s\n" % self.data)
+
+        request = self.data.decode()
+        request_info = request.split('\r\n')[0].split()
+
+        # fill the payload with an error if improper request or POST/DELETE/PUT
+        payload = self.__test_validity(request_info)
+
+        # handle get requests
+        if payload == b'':
+            payload = self.__prepare_response(request_info[1])
+
+        # send the payload
+        self.request.sendall(payload)
+
+    # return a payload with the requested file if it exists
+    def __prepare_response(self, directory):
+        # determine if the path provided exists
+        request_path = 'www' + directory
+        path_exists = os.path.exists(request_path)
+        response = b''
+        if path_exists:
+            is_file = os.path.isfile(request_path)
+            # handle location if it seems like a directory
+            if not is_file and request_path[-1] == '/':
+                request_path += 'index.html'
+
+                # ensure there is an index.html in this directory
+                is_file = os.path.isfile(request_path)
+                if not is_file:
+                    return b'HTTP/1.1 404 Not Found\r\n'
+
+            # handle directories requested without a '/'
+            elif not is_file and request_path[-1] != '/':
+                date = self.__date_header()
+                return b'HTTP/1.1 301 Moved Permanently\r\n' + date + b'Location: ' + directory.encode('utf-8') + b'/\r\n\r\n'
+
+            # get file contents
+            file = open(request_path)
+            contents = file.read().encode('utf-8')
+            file.close()
+
+            # get the headers
+            content_type = self.__content_type_header(
+                request_path.split('/')[-1])
+            date = self.__date_header()
+            length = b'Content Length: ' + \
+                str(len(contents)).encode('utf-8') + b'\r\n'
+
+            # create the response
+            response = b'HTTP/1.1 200 Ok\r\n' + date + \
+                content_type + length + b'\r\n' + contents
+            return response
+
+        else:
+            return b'HTTP/1.1 404 Not Found\r\n'
+
+    # determine if request is properly structured as 'GET' and not suspicious with location
+    def __test_validity(self, request_info):
+        if len(request_info) != 3:
+            return b'HTTP/1.1 400 Bad Request\r\n'
+        if request_info[0] != 'GET':
+            if request_info[0] == 'POST' or request_info[0] == 'DELETE' or request_info[0] == 'PUT':
+                return b'HTTP/1.1 405 Method Not Allowed\r\n'
+        if request_info[2] != 'HTTP/1.1':
+            return b'HTTP/1.1 400 Bad Request\r\n'
+
+        # block requests trying to exit www directory
+        path = request_info[1].split('/')
+        current_depth = 1
+        iter = 0
+        while iter < len(path) and current_depth > 0:
+            if path[iter] == '..':
+                current_depth -= 1
+            else:
+                current_depth += 1
+            iter += 1
+
+        if current_depth == 0:
+            return b'HTTP/1.1 404 Not Found'
+        return b''
+
+    # create header with date
+    def __date_header(self):
+        # https://pynative.com/python-get-the-day-of-week/#h-get-the-weekday-name-from-date-using-calendar-module
+        date = time.gmtime(time.time())
+        formatted_date = "{}, {} {} {} {}:{}:{} GMT".format(
+            calendar.day_name[date.tm_wday][0:3], date.tm_mday, calendar.month_name[date.tm_mon][0:3], date.tm_year, date.tm_hour, date.tm_min, date.tm_sec)
+        return b'Date: ' + formatted_date.encode('utf-8') + b'\r\n'
+
+    # generate content type header
+    def __content_type_header(self, file_name):
+        response = b'Content-Type:'
+        file_type = file_name.split('.')[-1]
+        if file_type == 'html':
+            return response + b' text/html\r\n'
+        elif file_type == 'css':
+            return response + b' text/css\r\n'
+        else:
+            return response + b' text\r\n'
+
 
 if __name__ == "__main__":
     HOST, PORT = "localhost", 8080
